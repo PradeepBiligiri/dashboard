59459cd76194027f188f2fbf9014e2b3e9a7bbfb
PradeepBiligiri <bhadripradeepbiligiri@gmail.com>
2022-09-11 09:41:57 +0530

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  faCaretDown,
+  faCaretUp,
+  faChartLine,
+  faCoins,
+  faDollar,
+  faSignal,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { LinearProgress } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import CircularProgress, {
+  circularProgressClasses,
+} from "@mui/material/CircularProgress";
+import { linearProgressClasses } from "@mui/material/LinearProgress";
+
+import "./App.css";
+import { green } from "@mui/material/colors";
+import { StackChart } from "./StackChart";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dashboard />
     </div>
   );
 }
 
 export default App;
+
+function Dashboard() {
+  return (
+    <div>
+      <SummaryBoxList />
+      <OverView />
+    </div>
+  );
+}
+function OverView() {
+  return (
+    <div className="overview-container">
+      <StackChart />
+    </div>
+  );
+}
+function SummaryBoxList() {
+  const dataList = [
+    {
+      number: 568,
+      percent: 0.7,
+      icon: faChartLine,
+      text: "Number Of Sales",
+      time: "Last Month",
+      iconColor: "rgb(135, 96, 251)",
+      performance: "up",
+      type: "count",
+    },
+    {
+      number: "12,897",
+      percent: 0.43,
+      icon: faCoins,
+      text: "New Revenue",
+      time: "Last Month",
+      iconColor: "#eb6f33",
+      performance: "down",
+      type: "money",
+    },
+    {
+      number: "11,234",
+      percent: 1.44,
+      icon: faDollar,
+      text: "Total Cost",
+      time: "Last Month",
+      iconColor: "#03c895",
+      performance: "down",
+      type: "money",
+    },
+    {
+      number: "789",
+      percent: 0.9,
+      icon: faSignal,
+      text: "Profit By Sale",
+      time: "Last Month",
+      iconColor: "#01b8ff",
+      performance: "up",
+      type: "money",
+    },
+  ];
+
+  return (
+    <div className="summary-box-list">
+      {dataList.map((data) => {
+        return <SummaryBar data={data} />;
+      })}
+    </div>
+  );
+}
+
+function SummaryBar({ data }) {
+  const CustomProgress = styled(LinearProgress)(({ theme }) => ({
+    height: 8,
+
+    [`&.${linearProgressClasses.colorPrimary}`]: {
+      backgroundColor:
+        theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
+    },
+    [`& .${linearProgressClasses.bar}`]: {
+      backgroundColor: data.iconColor,
+    },
+  }));
+  return (
+    <div className="summary-box-container">
+      <div className="summary-box-spec">
+        <p className="summary-box-text">{data.text}</p>
+        <FontAwesomeIcon style={{ color: data.iconColor }} icon={data.icon} />
+      </div>
+      <h2 className="summary-box-number">
+        {data.type === "money" ? "$" : ""}
+        {data.number}
+      </h2>
+      <CustomProgress variant="determinate" value={50} color="primary" />
+      <div className="summary-box-time-container">
+        <p>{data.time}</p>
+        <p className="ptag">
+          <FontAwesomeIcon
+            style={{ color: data.performance === "up" ? "green" : "red" }}
+            icon={data.performance === "up" ? faCaretUp : faCaretDown}
+          />{" "}
+          {data.percent}%
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/StackChart.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      fill: true,
+      lineTension: 0.2,
+      label: "Total Win",
+      data: [30, 100, 50, 150, 80, 250, 45, 200],
+      borderColor: "#583bab",
+      backgroundColor: "rgba(88, 58, 171 / 50% )",
+    },
+    {
+      fill: true,
+      lineTension: 0.2,
+      label: "Total Loss",
+      data: [100, 60, 120, 20, 250, 45, 100, 45],
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+export function StackChart() {
+  return <Line options={options} data={data} />;
+}
